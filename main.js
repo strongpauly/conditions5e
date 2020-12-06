aa0e84b7812e2de1c3b796246054054dfbc5a36a
Paul Potsides <paul@koble.ai>
2020-12-06 10:30:46 +0000

    Concentrating png.  Add some more of the spell effects.

--- main.js
+++ main.js
@@ -1,159 +1,187 @@
 /**
  * An array of status effect icons which can be applied to Tokens
  */
+
+const statusEffects = [
+  {
+    id: "dead",
+    label: "EFFECT.StatusDead",
+    icon: "modules/conditions5e/icons/dead.svg"
+  },
+  {
+    id: "unconscious",
+    label: "EFFECT.StatusUnconscious",
+    icon: "modules/conditions5e/icons/unconscious.svg"
+  },
+  {
+    id: "stun",
+    label: "EFFECT.StatusStunned",
+    icon: "modules/conditions5e/icons/stunned.svg"
+  },
+  {
+    id: "exhaustion1",
+    label: "EFFECT.StatusExhausted1",
+    icon: "modules/conditions5e/icons/exhaustion1.svg"
+  },
+  {
+    id: "incapacitated",
+    label: "EFFECT.StatusIncapacitated",
+    icon: "modules/conditions5e/icons/incapacitated.svg"
+  },
+  {
+    id: "paralysis",
+    label: "EFFECT.StatusParalysis",
+    icon: "modules/conditions5e/icons/paralyzed.svg",
+  },
+  {
+    id: "petrified",
+    label: "EFFECT.StatusPetrified",
+    icon: "modules/conditions5e/icons/petrified.svg",
+  },
+  {
+    id: "exhaustion2",
+    label: "EFFECT.StatusExhausted2",
+    icon: "modules/conditions5e/icons/exhaustion2.svg"
+  },
+  {
+    id: "grappled",
+    label: "EFFECT.StatusGrappled",
+    icon: "modules/conditions5e/icons/grappled.svg",
+  },
+  {
+    id: "restrain",
+    label: "EFFECT.StatusRestrained",
+    icon: "modules/conditions5e/icons/restrained.svg",
+  },
+  {
+    id: "prone",
+    label: "EFFECT.StatusProne",
+    icon: "modules/conditions5e/icons/prone.svg"
+  },
+  {
+    id: "exhaustion3",
+    label: "EFFECT.StatusExhausted3",
+    icon: "modules/conditions5e/icons/exhaustion3.svg"
+  },
+  {
+    id: "charmed",
+    label: "EFFECT.StatusCharmed",
+    icon: "modules/conditions5e/icons/charmed.svg"
+  },
+  {
+    id: "fear",
+    label: "EFFECT.StatusFear",
+    icon: "modules/conditions5e/icons/frightened.svg"
+  },
+  {
+    id: "poison",
+    label: "EFFECT.StatusPoison",
+    icon: "modules/conditions5e/icons/poisoned.svg"
+  },
+  {
+    id: "exhaustion4",
+    label: "EFFECT.StatusExhausted4",
+    icon: "modules/conditions5e/icons/exhaustion4.svg"
+  },
+  {
+    id: "blind",
+    label: "EFFECT.StatusBlind",
+    icon: "modules/conditions5e/icons/blinded.svg"
+  },
+  {
+    id: "deaf",
+    label: "EFFECT.StatusDeaf",
+    icon: "modules/conditions5e/icons/deafened.svg"
+  },
+  {
+    id: "disease",
+    label: "EFFECT.StatusDisease",
+    icon: "modules/conditions5e/icons/diseased.svg"
+  },
+  {
+    id: "exhaustion5",
+    label: "EFFECT.StatusExhausted5",
+    icon: "modules/conditions5e/icons/exhaustion5.svg"
+  },
+  {
+    id: "concentrating",
+    label: "EFFECT.StatusConcentrating",
+    icon: "modules/conditions5e/icons/concentrating.png"
+  },
+  {
+    id: "bane",
+    label: "EFFECT.SpellBlessed",
+    icon: "modules/conditions5e/icons/spells/1/bane.png"
+  },
+  {
+    id: "bless",
+    label: "EFFECT.SpellBane",
+    icon: "modules/conditions5e/icons/spells/1/bless.png"
+  },
+  {
+    id: "disguise-self",
+    label: "EFFECT.SpellDisguiseSelf",
+    icon: "modules/conditions5e/icons/spells/1/disguise-self.png"
+  },
+  {
+    id: "entangle",
+    label: "EFFECT.SpellEntangle",
+    icon: "modules/conditions5e/icons/spells/1/entangle.png"
+  },
+  {
+    id: "faerie-fire",
+    label: "EFFECT.SpellFaerieFire",
+    icon: "modules/conditions5e/icons/spells/1/faerie-fire.png"
+  },
+  {
+    id: "hex",
+    label: "EFFECT.SpellHex",
+    icon: "modules/conditions5e/icons/spells/1/hex.png"
+  },
+  {
+    id: "hunders-mark",
+    label: "EFFECT.SpellHuntersMark",
+    icon: "modules/conditions5e/icons/spells/1/hunters-mark.png"
+  },
+  {
+    id: "mage-armor",
+    label: "EFFECT.SpellMageArmor",
+    icon: "modules/conditions5e/icons/spells/1/mage-armor.png"
+  },
+  {
+    id: "protection-from-evil-and-good",
+    label: "EFFECT.SpellProtectionFromEvilAndGood",
+    icon: "modules/conditions5e/icons/spells/1/protection-from-evil-and-good.png"
+  },
+  {
+    id: "sanctuary",
+    label: "EFFECT.SpellSanctuary",
+    icon: "modules/conditions5e/icons/spells/1/sanctuary.png"
+  },
+  {
+    id: "shield-of-faith",
+    label: "EFFECT.SpellShieldOfFaith",
+    icon: "modules/conditions5e/icons/spells/1/shield-of-faith.png"
+  },
+  {
+    id: "shield",
+    label: "EFFECT.SpellShield",
+    icon: "modules/conditions5e/icons/spells/1/shield.png"
+  },
+  {
+    id: "tashas-hideous-laughter",
+    label: "EFFECT.SpellTashasHideousLaughter",
+    icon: "modules/conditions5e/icons/spells/1/tashas-hideous-laughter.png"
+  },
+];
+
 // Version before object conversion
 if(typeof CONFIG.statusEffects[0] === "string") {
-  CONFIG.statusEffects = [
-    "modules/conditions5e/icons/dead.svg",
-    "modules/conditions5e/icons/unconscious.svg",
-    "modules/conditions5e/icons/stunned.svg",
-    "modules/conditions5e/icons/exhaustion1.svg",
-  
-    "modules/conditions5e/icons/incapacitated.svg",
-    "modules/conditions5e/icons/paralyzed.svg",
-    "modules/conditions5e/icons/petrified.svg",
-    "modules/conditions5e/icons/exhaustion2.svg",
-  
-    "modules/conditions5e/icons/grappled.svg",
-    "modules/conditions5e/icons/restrained.svg",
-    "modules/conditions5e/icons/prone.svg",
-    "modules/conditions5e/icons/exhaustion3.svg",
-  
-    "modules/conditions5e/icons/charmed.svg",
-    "modules/conditions5e/icons/frightened.svg",
-    "modules/conditions5e/icons/poisoned.svg",
-    "modules/conditions5e/icons/exhaustion4.svg",
-  
-    "modules/conditions5e/icons/blinded.svg",
-    "modules/conditions5e/icons/deafened.svg",
-    "modules/conditions5e/icons/diseased.svg",
-    "modules/conditions5e/icons/exhaustion5.svg",
-
-    "modules/conditions5e/icons/concentrating.png"
-  ];
+  CONFIG.statusEffects = statusEffects.map(function(effect) {
+    return effect.icon
+  })
 } else {
-  CONFIG.statusEffects = [
-    {
-      id: "dead",
-      label: "EFFECT.StatusDead",
-      icon: "modules/conditions5e/icons/dead.svg"
-    },
-    {
-      id: "unconscious",
-      label: "EFFECT.StatusUnconscious",
-      icon: "modules/conditions5e/icons/unconscious.svg"
-    },
-    {
-      id: "stun",
-      label: "EFFECT.StatusStunned",
-      icon: "modules/conditions5e/icons/stunned.svg"
-    },
-    {
-      id: "exhaustion1",
-      label: "EFFECT.StatusExhausted1",
-      icon: "modules/conditions5e/icons/exhaustion1.svg"
-    },
-    {
-      id: "incapacitated",
-      label: "EFFECT.Incapacitated",
-      icon: "modules/conditions5e/icons/incapacitated.svg"
-    },
-    {
-      id: "paralysis",
-      label: "EFFECT.StatusParalysis",
-      icon: "modules/conditions5e/icons/paralyzed.svg",
-    },
-    {
-      id: "petrified",
-      label: "EFFECT.StatusPetrified",
-      icon: "modules/conditions5e/icons/petrified.svg",
-    },
-    {
-      id: "exhaustion2",
-      label: "EFFECT.StatusExhausted2",
-      icon: "modules/conditions5e/icons/exhaustion2.svg"
-    },
-    {
-      id: "grappled",
-      label: "EFFECT.StatusGrappled",
-      icon: "modules/conditions5e/icons/grappled.svg",
-    },
-    {
-      id: "restrain",
-      label: "EFFECT.StatusRestrained",
-      icon: "modules/conditions5e/icons/restrained.svg",
-    },
-    {
-      id: "prone",
-      label: "EFFECT.StatusProne",
-      icon: "modules/conditions5e/icons/prone.svg"
-    },
-    {
-      id: "exhaustion3",
-      label: "EFFECT.StatusExhausted3",
-      icon: "modules/conditions5e/icons/exhaustion3.svg"
-    },
-    {
-      id: "charmed",
-      label: "EFFECT.StatusCharmed",
-      icon: "modules/conditions5e/icons/charmed.svg"
-    },
-    {
-      id: "fear",
-      label: "EFFECT.StatusFear",
-      icon: "modules/conditions5e/icons/frightened.svg"
-    },
-    {
-      id: "poison",
-      label: "EFFECT.StatusPoison",
-      icon: "modules/conditions5e/icons/poisoned.svg"
-    },
-    {
-      id: "exhaustion4",
-      label: "EFFECT.StatusExhausted4",
-      icon: "modules/conditions5e/icons/exhaustion4.svg"
-    },
-    {
-      id: "blind",
-      label: "EFFECT.StatusBlind",
-      icon: "modules/conditions5e/icons/blinded.svg"
-    },
-    {
-      id: "deaf",
-      label: "EFFECT.StatusDeaf",
-      icon: "modules/conditions5e/icons/deafened.svg"
-    },
-    {
-      id: "disease",
-      label: "EFFECT.StatusDisease",
-      icon: "modules/conditions5e/icons/diseased.svg"
-    },
-    {
-      id: "exhaustion5",
-      label: "EFFECT.StatusExhausted5",
-      icon: "modules/conditions5e/icons/exhaustion5.svg"
-    },
-    {
-      id: "concentrating",
-      label: "EFFECT.StatusConcentrating",
-      icon: "modules/conditions5e/icons/concentrating.svg"
-    },
-    {
-      id: "bless",
-      label: "EFFECT.StatusBlessed",
-      icon: "modules/conditions5e/icons/spells/1/bless.png"
-    },
-    {
-      id: "faerie-fire",
-      label: "EFFECT.StatusFaerieFire",
-      icon: "modules/conditions5e/icons/spells/1/faerie-fire.png"
-    },
-    // {
-    //   id: "burning",
-    //   label: "EFFECT.StatusBurning",
-    //   icon: "icons/svg/fire.svg"
-    // },
-  ];
+  CONFIG.statusEffects = statusEffects
 }
 
 // Condition Types
